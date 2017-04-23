73244b53b33f8913980cac91a16cd53c947a5f98
Erik <erik.ra@gmail.com>
2017-04-23 14:35:11 +0200

    Add project files.

---
+++ R2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+
+            int r1 = int.Parse(inputs[0]);
+            int s = int.Parse(inputs[1]);
+
+            int r2 = (s * 2) - r1;
+
+            Console.WriteLine(r2);
+
+        }
+    }
+}
